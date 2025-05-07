eada43f5861591b2a4624d11ce7a8f2881a3a848
mikoonly <156190361+mikoonly@users.noreply.github.com>
2025-05-08 07:56:23 +0800

    Update __init__.py

--- pyrogram/handlers/__init__.py
+++ pyrogram/handlers/__init__.py
@@ -18,7 +18,7 @@
 
 
 from .callback_query_handler import CallbackQueryHandler
-#from .chat_join_request_handler import ChatJoinRequestHandler
+from .chat_join_request_handler import ChatJoinRequestHandler
 from .chat_member_updated_handler import ChatMemberUpdatedHandler
 from .chosen_inline_result_handler import ChosenInlineResultHandler
 from .deleted_messages_handler import DeletedMessagesHandler
@@ -27,7 +27,7 @@ from .edited_message_handler import EditedMessageHandler
 from .inline_query_handler import InlineQueryHandler
 from .message_handler import MessageHandler
 from .error_handler import ErrorHandler
-#from .poll_handler import PollHandler
+from .poll_handler import PollHandler
 from .raw_update_handler import RawUpdateHandler
 from .user_status_handler import UserStatusHandler
 #from .message_reaction_updated_handler import MessageReactionUpdatedHandler
