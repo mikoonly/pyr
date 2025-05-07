2e81da4b32134737cb8278d73dc7b81744391c86
mikoonly <156190361+mikoonly@users.noreply.github.com>
2025-05-08 07:19:25 +0800

    Update __init__.py

--- pyrogram/methods/calls/__init__.py
+++ pyrogram/methods/calls/__init__.py
@@ -1,1 +1,10 @@
+from .create_video_chat import CreateVideoChat
+from .discard_group_call import DiscardGroupCall
+from .get_video_chat_rtmp_url import GetVideoChatRtmpUrl
 
+
+class Calls:
+    CreateVideoChat,
+    DiscardGroupCall,
+):
+    pass
