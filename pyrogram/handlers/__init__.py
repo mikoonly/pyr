6678fea4de8484fb07da8566bf15f071cd13e633
mikoonly <156190361+mikoonly@users.noreply.github.com>
2025-05-08 07:55:51 +0800

    Update __init__.py

--- pyrogram/handlers/__init__.py
+++ pyrogram/handlers/__init__.py
@@ -15,3 +15,24 @@
 #
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from .callback_query_handler import CallbackQueryHandler
+#from .chat_join_request_handler import ChatJoinRequestHandler
+from .chat_member_updated_handler import ChatMemberUpdatedHandler
+from .chosen_inline_result_handler import ChosenInlineResultHandler
+from .deleted_messages_handler import DeletedMessagesHandler
+from .disconnect_handler import DisconnectHandler
+from .edited_message_handler import EditedMessageHandler
+from .inline_query_handler import InlineQueryHandler
+from .message_handler import MessageHandler
+from .error_handler import ErrorHandler
+#from .poll_handler import PollHandler
+from .raw_update_handler import RawUpdateHandler
+from .user_status_handler import UserStatusHandler
+#from .message_reaction_updated_handler import MessageReactionUpdatedHandler
+#from .message_reaction_count_updated_handler import MessageReactionCountUpdatedHandler
+#from .pre_checkout_query_handler import PreCheckoutQueryHandler
+#from .purchased_paid_media_handler import PurchasedPaidMediaHandler
+#from .shipping_query_handler import ShippingQueryHandler
+from .story_handler import StoryHandler
